35c532532b810bdaa189b776c8f5b6af10b9da78
Akhilesh-Shabadi <akhileshshabadi@gmail.com>
2026-02-02 23:48:04 +0530

    feat: Implement initial 3D portfolio structure including home page, sky model, and navigation bar with global styling.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,9 @@ import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <header className='header'>
+        <header className='flex justify-between items-center sm:px-16 px-8 py-4 max-w-5xl mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'>
             <NavLink to="/" className="w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md">
-                <p className='blue-gradient_text'>AS</p>
+                <p className='bg-linear-to-r from-[#00c6ff] to-[#0072ff] bg-clip-text text-transparent'>AS</p>
             </NavLink>
             <nav className='flex text-lg gap-7 font-medium'>
                 <NavLink to="/about" className={({ isActive }) => isActive ? "text-blue-500" : "text-gray-500"}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import { Suspense } from 'react'
+import { Suspense, useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import FoxIsland from '../models/FoxIsland'
+import Sky from '../models/Sky'
 
 const Home = () => {
     return (
@@ -16,17 +17,30 @@ const Home = () => {
 }
 
 function ModelHolder() {
+    const islandRef = useRef(null);
+
+    const screenResp = () => {
+        const scalePoint = window.innerWidth <= 768 ? 0.9 : 1;
+        const positionPoint = [-0.6, -5.8, -43];
+        const rotationPoint = [0.1, 4.7, 0];
+        return [scalePoint, positionPoint, rotationPoint]
+    }
+
+    const [islandScale, islandPosition, islandRotation] = screenResp();
     return (
         <Canvas className='w-full h-screen bg-transparent'
             camera={{ near: 0.1, far: 1000 }}
         >
             <Suspense fallback={<Loader />}>
-                <directionalLight />
-                <ambientLight />
-                <pointLight />
-                <spotLight />
-                <hemisphereLight />
-                <FoxIsland />
+                <directionalLight position={[1.3, 1, 1]} intensity={1.9} />
+                <ambientLight intensity={0.5} />
+                {/* <pointLight position={[1.3, 1, 1]} intensity={1.9} />
+                <spotLight position={[1.3, 1, 1]} intensity={1.9} /> */}
+                <hemisphereLight skyColor="#b1e1ff" groundColor={'#000000'} intensity={1} />
+
+                <Sky />
+
+                <FoxIsland scale={islandScale} position={islandPosition} rotation={islandRotation} islandRef={islandRef} />
             </Suspense>
         </Canvas>
     )
